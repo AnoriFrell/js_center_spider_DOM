9ef6f003905389cc774758bbd4ae4654764b71dd
Semen Sokolovskyi <anori.fjord@gmail.com>
2024-03-27 12:32:55 +0200

    make build

---
+++ index.548f788a.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=(t.clientHeight-e.clientHeight)/2,l=(t.clientWidth-e.clientWidth)/2;e.style.top="".concat(c,"px"),e.style.left="".concat(l,"px");
+//# sourceMappingURL=index.548f788a.js.map
